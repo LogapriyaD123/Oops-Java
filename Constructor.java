4eab1b508566a93e871afc45ae7abd8184b4e3e7
Logapriya D <logapriyad2005@gmail.com>
2026-06-08 22:57:58 +0530

    Add files via upload

---
+++ Constructor.java
@@ -0,0 +1,12 @@
+package Oops;
+
+class Student {
+    Student() {
+        System.out.println("Constructor Called");
+    }
+}
+public class Constructor{
+    public static void main(String[] args) {
+        Student s = new Student();
+    }
+}
