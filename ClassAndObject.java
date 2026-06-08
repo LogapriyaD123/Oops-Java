8cadbdc275bf31d4acba114f1375492ab25b6e8e
Logapriya D <logapriyad2005@gmail.com>
2026-06-08 22:36:30 +0530

    Add files via upload

---
+++ ClassAndObject.java
@@ -0,0 +1,11 @@
+package Oops;
+class Student {
+    String name;
+}
+public class ClassAndObject {
+    public static void main(String[] args) {
+        Student s = new Student();
+        s.name = "Logapriya";
+        System.out.println("Name: " + s.name);
+    }
+}
